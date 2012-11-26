b45c79eac4817c447216a0166ca39dfe56d34f5c
buxiangshuo <moshuohua@gmail.com>
2012-11-26 21:31:36 +0800

    add some files
    添加了一些文件，主要是custom.css

---
+++ assets/js/timeline.js
@@ -0,0 +1,29 @@
+/*
+ 
+ @authors leon <kairyou@qq.com>
+          you
+*/
+$(function(){
+
+function Arrow_Points() { 
+	var s = $('#container').find('.item');
+	$.each(s,function(i,obj){
+		var posLeft = $(obj).css("left");
+		$(obj).addClass('borderclass');
+		if(posLeft == "0px") {
+			html = "<span class='rightCorner'></span>";
+			$(obj).prepend(html); 
+		}
+		else
+		{
+			html = "<span class='leftCorner'></span>";
+			$(obj).prepend(html);
+		}
+	});
+}
+
+// Divs
+$('#container').masonry({itemSelector : '.item',});
+	Arrow_Points();
+});
+
