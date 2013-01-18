ee682dc07a98f1e5b0c95de54728505da4cb7240
buxiangshuo <moshuohua@gmail.com>
2013-01-18 20:25:25 +0800

    add header and footer
    import google-bootstrap file for the main style and js components

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -14,7 +14,9 @@ class Home extends CI_Controller {
 
     public function index()
     {
-        $this->load->view('home');
+        $this->load->view('templates/header');
+  		$this->load->view('home');
+  		$this->load->view('templates/footer');
     }
 
 }
