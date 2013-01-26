ec2d0ae134ce15b90960a62558f8d9cba57bf679
buxiangshuo <moshuohua@gmail.com>
2013-01-27 00:00:53 +0800

    don't use drap model

--- application/views/project/issues.php
+++ application/views/project/issues.php
@@ -1,27 +1,8 @@
 <div class="container">
 	<div class="row">
 		<hr>
-		<div class="span12">
-			<div class="gridster">
-			    <ul>
-			        <li data-row="1" data-col="1" data-sizex="1" data-sizey="1"></li>
-			        <li data-row="2" data-col="1" data-sizex="1" data-sizey="1"></li>
-			        <li data-row="3" data-col="1" data-sizex="1" data-sizey="1"></li>
- 
-			        <li data-row="1" data-col="2" data-sizex="2" data-sizey="1"></li>
-			        <li data-row="2" data-col="2" data-sizex="2" data-sizey="2"></li>
- 
-			        <li data-row="1" data-col="4" data-sizex="1" data-sizey="1"></li>
-			        <li data-row="2" data-col="4" data-sizex="2" data-sizey="1"></li>
-			        <li data-row="3" data-col="4" data-sizex="1" data-sizey="1"></li>
- 
-			        <li data-row="1" data-col="5" data-sizex="1" data-sizey="1"></li>
-			        <li data-row="3" data-col="5" data-sizex="1" data-sizey="1"></li>
- 
-			        <li data-row="1" data-col="6" data-sizex="1" data-sizey="1"></li>
-			        <li data-row="2" data-col="6" data-sizex="1" data-sizey="2"></li>
-			    </ul>
-			</div>
+		<div class="span11 well">
+			
 		</div>
 	</div>
 </div>	
