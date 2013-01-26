6a3c1095acef121059605084bd978be3d15def2c
buxiangshuo <moshuohua@gmail.com>
2013-01-27 02:45:35 +0800

    add docs and issue button

--- application/views/project/issues.php
+++ application/views/project/issues.php
@@ -1,6 +1,9 @@
 <div class="container">
 	<div class="row">
 		<hr>
-		
+		<div class="btn-group" data-toggle="buttons-radio">
+			<button class="btn btn-success"><i class="icon-file icon-white"></i> Docs</button>
+			<button class="btn btn-success"><i class="icon-list icon-white"></i> Issues</button>
+		</div>
 	</div>
 </div>	
