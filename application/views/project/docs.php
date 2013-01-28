81cf6177d64ba133814b1f8c905f28af766d5ed8
buxiangshuo <moshuohua@gmail.com>
2013-01-28 23:59:00 +0800

    add table for docs

--- application/views/project/docs.php
+++ application/views/project/docs.php
@@ -4,7 +4,7 @@
 	<div class="row">
 		<div class="span12 inline">
       <div class="inline">
-        <div class="btn-group" data-toggle="buttons-radio" style="margin-left:277px !important;">
+        <div class="btn-group" data-toggle="buttons-radio" style="margin-left:271px !important;">
           <a href="#" class="btn btn-primary" rel="popover" title="Project one" data-placement="bottom" data-content="And here's some amazing content. It's very engaging. right?And here's some amazing content. It's very engaging. right?And here's some amazing content. It's very engaging. right?And here's some amazing content. It's very engaging. right?"><i class="icon-leaf icon-white"></i> Project</a>
           <button class="btn btn-primary"><i class="icon-file icon-white"></i> Docs</button>
           <button class="btn btn-primary"><i class="icon-list icon-white"></i> Issue</button>
@@ -17,8 +17,32 @@
 <!-- --------------the main part-------------- -->
     <div class="span12">
       <hr>
+      <table class="table table-bordered table-hover">
+              <thead>
+                <tr>
+                  <th class="span1"></th>
+                  <th class="span3">NAME</th>
+                  <th class="span7">DESCRIPTION</th>
+                  <th>CREATOR</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>And here's some amazing content. </td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+              </tbody>
+            </table>
     </div>
 
-    
+
   </div>
 </div>
