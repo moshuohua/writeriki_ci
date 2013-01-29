1e3fc34c1a0240f81ce24cb16fd99086d9bcd095
buxiangshuo <moshuohua@gmail.com>
2013-01-30 02:08:32 +0800

    use contenteditable

--- application/views/project/single_open_doc.php
+++ application/views/project/single_open_doc.php
@@ -3,7 +3,17 @@
 	<div class="span12">
 		<hr>
 		<div contenteditable="true">
+			<h1>hello world</h1>
+			<p>This sample page demonstrates the inline editing feature - CKEditor instances will be created automatically from page elements with <strong>contentEditable</strong> attribute set to value <strong>true</strong>:</p>
 			<p>This sample page demonstrates the inline editing feature - CKEditor instances will be created automatically from page elements with <strong>contentEditable</strong> attribute set to value <strong>true</strong>:</p>
 		</div>
+		<div contenteditable="true">
+				<p>
+					Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies.
+				</p>
+				<p>
+					Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna. Vestibulum dapibus, mauris nec malesuada fames ac.
+				</p>
+			</div>
 	</div>
 	
