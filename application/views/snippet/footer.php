736f4d04a3606a3853df9967b627a4d67e44f139
buxiangshuo <moshuohua@gmail.com>
2013-01-31 20:28:47 +0800

    fix the js file url

--- application/views/snippet/footer.php
+++ application/views/snippet/footer.php
@@ -15,30 +15,15 @@
     </div>
 </div>
 
-    <script src="<?=base_url();?>assets/js/wysihtml5-0.3.0.js"></script>
     <script src="<?=base_url();?>assets/js/jquery.js"></script>
     <script src="<?=base_url();?>assets/js/bootstrap.min.js"></script>
-    <script src="<?=base_url();?>assets/js/bootstrap-transition.js"></script>
-    <script src="<?=base_url();?>assets/js/bootstrap-alert.js"></script>
-    <script src="<?=base_url();?>assets/js/bootstrap-modal.js"></script>
-    <script src="<?=base_url();?>assets/js/bootstrap-dropdown.js"></script>
-    <script src="<?=base_url();?>assets/js/bootstrap-scrollspy.js"></script>
-    <script src="<?=base_url();?>assets/js/bootstrap-tab.js"></script>
-    <script src="<?=base_url();?>assets/js/bootstrap-tooltip.js"></script>
-    <script src="<?=base_url();?>assets/js/bootstrap-popover.js"></script>
-    <script src="<?=base_url();?>assets/js/bootstrap-button.js"></script>
-    <script src="<?=base_url();?>assets/js/bootstrap-collapse.js"></script>
-    <script src="<?=base_url();?>assets/js/bootstrap-carousel.js"></script>
-    <script src="<?=base_url();?>assets/js/bootstrap-typeahead.js"></script>
-    <script src="<?=base_url();?>assets/js/bootstrap-affix.js"></script>
-
     <script src="<?=base_url();?>assets/js/holder/holder.js"></script>
     <script src="<?=base_url();?>assets/js/google-code-prettify/prettify.js"></script>
     <script src="<?=base_url();?>assets/js/select2.min.js"></script>
 
     <script src="<?=base_url();?>assets/js/application.js"></script>
 
-    
+    <script src="<?=base_url();?>assets/js/wysihtml5-0.3.0.js"></script> 
     <script src="<?=base_url();?>assets/js/bootstrap-wysihtml5.js"></script>
     
 <!-- -- this is for the bootstrap tooltip ------------------------------- -->
