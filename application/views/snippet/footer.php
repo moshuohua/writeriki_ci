51e3c2d3ac251d895d9438f7e80275d598e3cd6a
buxiangshuo <moshuohua@gmail.com>
2013-02-07 03:34:43 +0800

    add modal for todo mail notice

--- application/views/snippet/footer.php
+++ application/views/snippet/footer.php
@@ -4,7 +4,7 @@
       <div class="navbar-inner">
         <div class="container">
             <div class="btn-group pull-right" data-toggle="buttons-radio">
-              <a class="btn btn-primary" href="#"><i class="icon-leaf icon-white"></i> Todo</a>
+              <a class="btn btn-primary" href="#Todolist" data-toggle="modal"><i class="icon-leaf icon-white"></i> Todo</a>
               <a class="btn btn-primary" href="#"><i class="icon-envelope icon-white"></i> Mail</a>
               <a class="btn btn-primary" href="#"><i class="icon-bell icon-white"></i> Notice</a>
             </div>
@@ -14,6 +14,54 @@
 
     </div>
 </div>
+
+<!-- ----------------todolist modal---------------- -->
+  <div id="Todolist" class="modal hide fade" tabindex="-1" role="dialog" 
+      aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-header">
+      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+      <h3 id="myModalLabel">Modal header</h3>
+    </div>
+    <div class="modal-body">
+      <p>One fine body…</p>
+    </div>
+    <div class="modal-footer">
+      <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+      <button class="btn btn-primary">Save changes</button>
+    </div>
+  </div>
+
+<!-- ----------------mail modal---------------- -->
+  <div id="sitemail" class="modal hide fade" tabindex="-1" role="dialog" 
+      aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-header">
+      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+      <h3 id="myModalLabel">Modal header</h3>
+    </div>
+    <div class="modal-body">
+      <p>One fine body…</p>
+    </div>
+    <div class="modal-footer">
+      <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+      <button class="btn btn-primary">Save changes</button>
+    </div>
+  </div>
+
+<!-- ----------------notify modal---------------- -->
+  <div id="Notice" class="modal hide fade" tabindex="-1" role="dialog" 
+      aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-header">
+      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+      <h3 id="myModalLabel">Modal header</h3>
+    </div>
+    <div class="modal-body">
+      <p>One fine body…</p>
+    </div>
+    <div class="modal-footer">
+      <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+      <button class="btn btn-primary">Save changes</button>
+    </div>
+  </div>
 
     <script src="<?=base_url();?>assets/js/jquery.js"></script>
     <script src="<?=base_url();?>assets/js/bootstrap.min.js"></script>
