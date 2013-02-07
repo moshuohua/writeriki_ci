aac6522e5f9a169475ca86ee02b65a25b88d0e99
buxiangshuo <moshuohua@gmail.com>
2013-02-08 01:24:59 +0800

    fix the nav

--- application/views/snippet/nav.php
+++ application/views/snippet/nav.php
@@ -24,26 +24,43 @@
                 <a href="#">日历</a>
               </li>
               <li class="">
-                <a href="#">综合</a>
-              </li>
-              <li class="" style="border-right:1px solid #ddd;">
                 <a href="#">团队 </a>
               </li>
+              <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">资源<b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li class="divider"></li>
+                  <li><a href="#">查看所有文档</a></li>
+                  <li><a href="#">查看所有事务</a></li>
+                  <li><a href="#">查看所有文件</a></li>
+                  <li><a href="#">查看所有讨论</a></li>
+                  <li><a href="#">查看所有Todo</a></li>
+                  <li><a href="#">查看所有私信</a></li>
+                  <li class="divider"></li>
+                </ul>
+              </li>
+              <li class="divider-vertical"></li>
+              <li class="">
+                <form class="form-search">
+                  <div class="navbar-search input-append pull-right">
+                    <input type="text" class="span3 search-query">
+                    <button type="submit" class="btn btn-success">搜索</button>
+                  </div>
+                </form>
+              </li>
+            </ul>
+            <ul class="nav pull-right">
+              <li class="divider-vertical"></li>
               <li class="">
                 <a href="#"> 注册</a>
               </li>
               <li class="">
                 <a href="#"> 登陆</a>
               </li>
+              <li class="divider-vertical"></li>
             </ul>
           </div>
-          <form class="form-search">
-            <div class="navbar-search input-append pull-right">
-              <input type="text" class="span3 search-query">
-              <button type="submit" class="btn btn-success">搜索</button>
-            </div>
-          </form>
         </div>
       </div>
 </div>
-
+<hr style="margin:-12px 0px 0px 12px;">
