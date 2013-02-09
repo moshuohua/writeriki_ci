c35d4af95da230a70243d08c1c491a5a3e827d0a
buxiangshuo <moshuohua@gmail.com>
2013-02-10 02:31:49 +0800

    fix the timeline style

--- application/views/process/index.php
+++ application/views/process/index.php
@@ -1,5 +1,6 @@
 
 <!-- --------------the main part-------------- -->
+<div class="span12 well" style="margin :0px;"></div>
     <div class="span12">
       <div id="timebox">
         <!-- timeline container -->
@@ -11,5 +12,7 @@
         <div class="item">23helloh3hellohellohellohellohellohellohellohellohellohellohellohellohello3hellohellohellohellohellohellohellohellohellohellohellohellohelloellohellohellohellohellohellohellohellohellohellohellohello3hellohellohellohellohellohellohellohellohellohellohellohellohello</div>
         <div class="item">3hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello</div>
         <div class="item">ellohellohellohellohellohello</div>
+        <div class="item">23helloh3hellohellohellohellohellohellohellohellohellohellohellohellohello3hellohellohellohellohellohellohellohellohellohellohellohellohelloellohellohellohellohellohellohellohellohellohellohellohello3hellohellohellohellohellohellohellohellohellohellohellohellohello</div>
+        <div class="item">23helloh3hellohellohellohellohellohellohellohellohellohellohellohellohello3hellohellohellohellohellohellohellohellohellohellohellohellohelloellohellohellohellohellohellohellohellohellohellohellohello3hellohellohellohellohellohellohellohellohellohellohellohellohello</div>
       </div>
     </div>
