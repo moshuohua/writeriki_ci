950c193e6fd5b75605b28c163b891241ab6c8702
buxiangshuo <moshuohua@gmail.com>
2013-02-11 00:02:29 +0800

    big change for process （useing media list style）

--- application/views/process/index.php
+++ application/views/process/index.php
@@ -1,18 +1,47 @@
 
 <!-- --------------the main part-------------- -->
-<div class="span12 well" style="margin :0px;"></div>
-    <div class="span12">
-      <div id="timebox">
-        <!-- timeline container -->
-        <div class="timeline_container">
-          <div class="timeline"></div>
-        </div>
-        <!-- items -->
-        <div class="item">13hellohellohellohellohellohellohellohellohellohellohellohellohello3hellohellohellohellohellohellohellohellohellohellohellohellohello3hellohellohellohellohellohellohellohellohellohellohellohellohello</div>
-        <div class="item">23helloh3hellohellohellohellohellohellohellohellohellohellohellohellohello3hellohellohellohellohellohellohellohellohellohellohellohellohelloellohellohellohellohellohellohellohellohellohellohellohello3hellohellohellohellohellohellohellohellohellohellohellohellohello</div>
-        <div class="item">3hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello</div>
-        <div class="item">ellohellohellohellohellohello</div>
-        <div class="item">23helloh3hellohellohellohellohellohellohellohellohellohellohellohellohello3hellohellohellohellohellohellohellohellohellohellohellohellohelloellohellohellohellohellohellohellohellohellohellohellohello3hellohellohellohellohellohellohellohellohellohellohellohellohello</div>
-        <div class="item">23helloh3hellohellohellohellohellohellohellohellohellohellohellohellohello3hellohellohellohellohellohellohellohellohellohellohellohellohelloellohellohellohellohellohellohellohellohellohellohellohello3hellohellohellohellohellohellohellohellohellohellohellohellohello</div>
-      </div>
+    <hr>
+<div class="span12">
+    <div class="item">
+      <!-- ------time stamp------ -->
+        <ul class="nav nav-tabs nav-tabs-google">
+            <li class="active">
+                <a href="#">2013 Mars 9</a>
+            </li>
+        </ul>
+        <ul class="media-list">
+            <li class="media">
+                <a class="pull-left" href="#">
+                  <img class="media-object" data-src="holder.js/64x64">
+                </a>
+                <div class="media-body">
+                    <h4 class="media-heading">Media heading</h4>
+                    <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
+                    <div class="media">
+                        <div class="media-body">
+                            <h4 class="media-heading">Nested media heading</h4>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+                        </div>
+                    </div>
+                </div>
+            </li>
+    <!-- ------anther person actions----- -->
+            <li class="media">
+                <a class="pull-left" href="#">
+                  <img class="media-object" data-src="holder.js/64x64">
+                </a>
+                <div class="media-body">
+                    <h4 class="media-heading">Media heading</h4>
+                    <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
+                    <div class="media">
+                        <div class="media-body">
+                            <h4 class="media-heading">Nested media heading</h4>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+      <!-- ------end process line------ -->
     </div>
+</div>
